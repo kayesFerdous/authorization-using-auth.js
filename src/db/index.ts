94742302b3ed03f2b69d79c2b2bfd51a3d84a846
kayesFerdous <kayesfardows@gmail.com>
2024-09-21 18:21:10 +0600

    connet to vercel/postgres

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,4 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { sql } from "@vercel/postgres";
 
-const POSTGRES_URL = process.env.POSTGRES_URL as string;
-
-const queryClient = postgres(POSTGRES_URL);
-export const db = drizzle(queryClient);
-
-const migrationClient = postgres(POSTGRES_URL, { max: 1 });
-migrate(drizzle(migrationClient), { migrationsFolder: "./migrations" });
+export default db = drizzle(sql);
