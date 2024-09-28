5e23a9734a30a6a6d4a84aa16b3e8c9f6dff1d09
kayesFerdous <kayesfardows@gmail.com>
2024-09-28 13:08:06 +0600

    lol

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,36 @@
 import NextAuth from "next-auth";
 import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import { getUserByEmail, insertUser } from "./lib/dbOperations";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      async profile(profile) {
+        const user = await getUserByEmail(profile.email!);
+
+        if (user?._id) {
+          return {
+            _id: user._id,
+            name: profile.name,
+            email: user.email,
+            image: profile.avatar_url,
+            isAdmin: user.isAdmin,
+          };
+        } else {
+          const user = await insertUser(profile.email!);
+          return {
+            _id: user?._id,
+            name: profile.name,
+            email: profile.email,
+            image: profile.avatar_url,
+            isAdmin: user?.isAdmin,
+          };
+        }
+      },
+    }),
     Github({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
